e3869fd021441071b5e9c15eaa88c9b0a0fd66d3
Hoff <haffedmcnair@gmail.com>
2019-10-23 18:19:26 -0700

    did a lot. these r bad commit messages.

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,60 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
+import Button from '@material-ui/core/Button'
+import "./App.css";
+
+
+
+class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {isSignedIn: false}
+  }
+  
+  // Configure FirebaseUI.
+  uiConfig = {
+      // Popup signin flow rather than redirect flow.
+      signInFlow: 'popup',
+      // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+      signInSuccessUrl: '/signedIn',
+      // We will display Google and Facebook as auth providers.
+      signInOptions: [
+        this.props.fireBase.auth.EmailAuthProvider.PROVIDER_ID,
+        this.props.fireBase.auth.GoogleAuthProvider.PROVIDER_ID,
+        this.props.fireBase.auth.FacebookAuthProvider.PROVIDER_ID,
+        this.props.fireBase.auth.TwitterAuthProvider.PROVIDER_ID
+      ]
+    };
+
+
+
+
+  componentDidMount(){
+    this.props.fireBase.auth().onAuthStateChanged(user => {
+      this.setState({isSignedIn:!!user})
+    })
+  }
+
+  render(){
+    return(
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <div>
+          <div>Signed In!</div>
+          <Button variant ="contained" color="primary" onClick={()=>this.props.fireBase.auth().signOut()}>Sign Out!</Button>
+          <h1>Welcome {this.props.fireBase.auth().currentUser.displayName}</h1>
+          </div>
+        ) : (
+          <StyledFirebaseAuth 
+            uiConfig={this.uiConfig}
+            firebaseAuth={this.props.fireBase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
 }
 
 export default App;
--- client/src/index.js
+++ client/src/index.js
@@ -2,9 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import firebase from 'firebase'
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+let firebaseConfig = {
+    apiKey: "AIzaSyA6RIfEDo0UjDG7KHbCaMPEIGgrRcEdvVA",
+    authDomain: "micnight-9b383.firebaseapp.com",
+    databaseURL: "https://micnight-9b383.firebaseio.com",
+    projectId: "micnight-9b383",
+    storageBucket: "micnight-9b383.appspot.com",
+    messagingSenderId: "318590627497",
+    appId: "1:318590627497:web:0b4d207c91bc0cb8cd83b8",
+    measurementId: "G-W7Y1FZG0PH"
+  };
+
+  firebase.initializeApp(firebaseConfig)
+
+ReactDOM.render(<App fireBase={firebase}/>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
