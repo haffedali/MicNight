2be40c3ed7661425e7dc32490fb9cadbb95fb2b5
Hoff <haffedmcnair@gmail.com>
2019-11-26 19:41:28 -0800

    pulled CurrentArtistDisplay to composite components and started building it

--- client/src/composite-components/CurrentArtistDisplay/CurrentArtistDisplay.js
+++ client/src/composite-components/CurrentArtistDisplay/CurrentArtistDisplay.js
@@ -3,8 +3,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 
-import ProfileAvatar from '../ProfileAvatar';
-import ProfileCard from '../../composite-components/ProfileCard';
+import ProfileAvatar from '../../components/ProfileAvatar';
+import ProfileCard from '../ProfileCard';
 import SocialLinks from '../../components/SocialLinks';
 
 const CurrentArtistDisplay = () => (
