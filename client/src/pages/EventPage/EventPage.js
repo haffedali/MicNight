2c44f30d464134ca2442adfd46f492090a044e8a
Hoff <haffedmcnair@gmail.com>
2019-11-26 19:30:26 -0800

    event page now displays and is sized to fit the screen

--- client/src/pages/EventPage/EventPage.js
+++ client/src/pages/EventPage/EventPage.js
@@ -1,7 +1,28 @@
 import React from 'react';
+import {makeStyles} from '@material-ui/core/styles';
+import { Container } from '@material-ui/core';
+
 
 const EventPage = () => {
-  return <div>Event Page</div>;
+  const useStyles = makeStyles({
+    container: {
+      width: '100vw',
+      minHeight: '92vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      backgroundColor: '#f1f7f3'
+    }
+  })
+
+  const classes = useStyles();
+
+  return(
+    <Container className={classes.container}>
+
+    </Container>
+  ) 
+;
 };
 
 export default EventPage;
