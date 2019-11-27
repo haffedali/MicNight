4b699f9020ecf8473a927f43cbbe15f5b0f65927
Hoff <haffedmcnair@gmail.com>
2019-11-26 19:40:05 -0800

    started building out CurrentArtistDisaply

--- client/src/components/CurrentArtistDisplay/CurrentArtistDisplay.js
+++ client/src/components/CurrentArtistDisplay/CurrentArtistDisplay.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
-import ProfileAvatar from '../ProfileAvatar';
 import Box from '@material-ui/core/Box';
 import Paper from '@material-ui/core/Paper';
 
+import ProfileAvatar from '../ProfileAvatar';
+import ProfileCard from '../../composite-components/ProfileCard';
+import SocialLinks from '../../components/SocialLinks';
+
 const CurrentArtistDisplay = () => (
-    <ProfileAvatar photoUrl={'https://avatars.dicebear.com/v2/avataaars/Haffed.svg'} height='30vw' width='30vw'/>
+   <Paper>
+       <ProfileCard />
+       <SocialLinks />
+   </Paper>
 )
 
 export default CurrentArtistDisplay
