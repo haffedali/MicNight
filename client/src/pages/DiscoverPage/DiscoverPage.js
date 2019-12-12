c5c384bab80762b980b091ef00ed968a8a0d0fef
Hoff <haffedmcnair@gmail.com>
2019-12-12 15:33:31 -0800

    added page CSS

--- client/src/pages/DiscoverPage/DiscoverPage.js
+++ client/src/pages/DiscoverPage/DiscoverPage.js
@@ -1,10 +1,27 @@
 import React, { useState } from 'react';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import { Container, Grid } from '@material-ui/core';
 import MapBox from '../../components/MapBox'
 
-const DiscoverPage = () => {
-  return (<div><MapBox /></div>)
+const DiscoverPage = (props) => {
+
+  const useStyles = makeStyles({
+    container: {
+      width: '100vw',
+      minHeight: '92vh',
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'space-between',
+      backgroundColor: '#f1f7f3'
+    }
+  });
+
+  const classes = useStyles();
+  return (
+    <Container className={classes.container}>
+      <MapBox />
+    </Container>
+  )
 };
 
 export default DiscoverPage;
