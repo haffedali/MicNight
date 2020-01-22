c0119543954d684c0191f5c820f95611ef47b74b
Hoff <haffedmcnair@gmail.com>
2020-01-21 17:04:58 -0800

    halfway through linting my components

---
+++ client/src/components/DataTableItem/DataTableItem.jsx
@@ -0,0 +1,98 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+// import List from '@material-ui/core/List';
+import Box from '@material-ui/core/Box';
+import ListItem from '@material-ui/core/ListItem';
+import ListItemAvatar from '@material-ui/core/ListItemAvatar';
+import ListItemSecondaryAction from '@material-ui/core/ListItemSecondaryAction';
+import ListItemText from '@material-ui/core/ListItemText';
+import Paper from '@material-ui/core/Paper';
+import { makeStyles } from '@material-ui/styles';
+
+
+import ProfileAvatar from '../ProfileAvatar';
+import SocialLinks from '../SocialLinks';
+
+
+const DataTableItem = ({ entry }) => {
+  const useStyles = makeStyles({
+    root: {
+      paddingLeft: '3vw',
+      margin: '.5vw',
+    },
+    buttonBox: {
+      height: '12%',
+      width: '15vw',
+    },
+    detailBox: {
+
+    },
+    socialLink: {
+      padding: '.1vw',
+      margin: '.1vw',
+    },
+    listItem: {
+
+    },
+    avatar: {
+      margin: '2%',
+      minWidth: '1vw',
+    },
+    listSecondaryAction: {
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+    },
+  });
+
+  const classes = useStyles();
+
+  return (
+    <Paper className={classes.root}>
+      <ListItem mb={1} disableGutters divider className={classes.listItem} key={`ListItem: ${entry.uid}`}>
+        <ListItemAvatar className={classes.avatar}>
+          <ProfileAvatar photoUrl={entry.photoUrl} />
+        </ListItemAvatar>
+        <Box className={classes.detailBox}>
+          <ListItemText
+            primary={entry.name}
+            secondary={entry.tagLine}
+          />
+        </Box>
+
+        <ListItemSecondaryAction className={classes.listSecondaryAction} style={{ height: '100%', width: '20%' }}>
+          {/* Refractor into 'SocialLinks' component */}
+
+          <SocialLinks />
+
+
+          {/* Refractor into 'SocialLinks' component */}
+
+        </ListItemSecondaryAction>
+
+      </ListItem>
+    </Paper>
+
+  );
+};
+
+DataTableItem.propTypes = {
+  entry: PropTypes.shape({
+    uid: PropTypes.string,
+    photoUrl: PropTypes.string,
+    name: PropTypes.string,
+    tagLine: PropTypes.string,
+  }),
+};
+
+DataTableItem.defaultProps = {
+  entry: {
+    uid: 1234,
+    photoUrl: 'https://avatars.dicebear.com/v2/avataaars/Haffed.svg',
+    name: 'Haffed Ali',
+    tagLine: 'Maker Mover',
+  },
+};
+
+export default DataTableItem;
---
+++ client/src/components/DataTableViewController/DataTableViewController.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import Button from '@material-ui/core/Button';
+import ButtonGroup from '@material-ui/core/ButtonGroup';
+import Container from '@material-ui/core/Container';
+import { makeStyles } from '@material-ui/core/styles';
+
+
+const DataTableViewController = ({ buttons, handleFocusChange }) => {
+  const useStyles = makeStyles({
+    buttonGroup: {
+
+    },
+    container: {
+      display: 'flex',
+      justifyContent: 'center',
+      padding: '1vw',
+      maxWidth: 'lg',
+    },
+    viewControls: {
+
+    },
+    button: {
+      paddingLeft: '3vw',
+      paddingRight: '3vw',
+    },
+
+  });
+
+
+  const classes = useStyles();
+
+
+  return (
+    <Container className={classes.container}>
+      <ButtonGroup
+        className={classes.viewControls}
+        variant="text"
+        color="secondary"
+        size="medium"
+      >
+        {buttons.map((button, x) => (
+          <Button onClick={handleFocusChange} className={classes.button} value={`DataTableController ${x}`}>
+            {button}
+          </Button>
+        ))}
+      </ButtonGroup>
+
+    </Container>
+  );
+};
+
+export default DataTableViewController;
