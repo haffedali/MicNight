b277c5d99e5a506858c433e1546d4dc29b9cb7a8
Hoff <haffedmcnair@gmail.com>
2020-01-25 10:59:29 -0800

    linted some files so far... still a good amount to go

--- client/src/components/LogInUI/LogInUI.js
+++ client/src/components/LogInUI/LogInUI.js
@@ -1,54 +1,55 @@
 import React, { Component } from 'react';
-import AuthenticationContext from '../AuthenticationContext';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
-import Button from '@material-ui/core/Button'
+import AuthenticationContext from '../AuthenticationContext';
 
 class LogInUI extends Component {
-  constructor(props) {
-    super(props)
-    this.state = { isSignedIn: false }
+  constructor({ firebase }) {
+    super(firebase);
+    console.log(firebase)
+    this.uiConfig = {
+      // Popup signin flow rather than redirect flow.
+      signInFlow: 'popup',
+      signInOptions: [
+        firebase.auth.EmailAuthProvider.PROVIDER_ID,
+        firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+      ],
+      callbacks: {
+      },
+    };
   }
 
-  uiConfig = {
-    // Popup signin flow rather than redirect flow.
-    signInFlow: 'popup',
-    signInOptions: [
-      this.props.firebase.auth.EmailAuthProvider.PROVIDER_ID,
-      this.props.firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    ],
-    callbacks: {
-    }
-  }
 
   componentDidMount() {
-    this.props.firebase.auth().onAuthStateChanged(user => {
-      this.setState({ isSignedIn: !!user })
-    })
+    this.firebase.auth().onAuthStateChanged((user) => {
+      this.setState({ isSignedIn: !!user });
+    });
   }
 
-  componentDidUpdate(){
+  componentDidUpdate() {
 
   }
 
   render() {
     return (
       <AuthenticationContext.Consumer>
-        {({ isAuthenticated, userInfo, authenticateUser, deAuthenticateUser }) => (
+        {({
+          isAuthenticated, userInfo, authenticateUser, deAuthenticateUser,
+        }) => (
           <div className="login-container">
             {/* Super questionable attachment of my authentication method here, gets a red warning */}
             {this.uiConfig.callbacks.signInSuccessWithAuthResult = authenticateUser}
 
-                <div>
-                  <StyledFirebaseAuth
-                    uiConfig={this.uiConfig}
-                    firebaseAuth={this.props.firebase.auth()}
-                  />
-                </div>
+            <div>
+              <StyledFirebaseAuth
+                uiConfig={this.uiConfig}
+                firebaseAuth={this.firebase.auth()}
+              />
+            </div>
           </div>
         )}
       </AuthenticationContext.Consumer>
 
-    )
+    );
   }
 }
 
